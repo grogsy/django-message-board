55951e0b08c23447f77dcc13d993e08b2077e719
Brian <a.gray.cloud@gmail.com>
2019-12-02 03:33:07 -0400

    Add author field to models.py

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=54, default='New Post')
+    author = models.CharField(max_length=16, default="Anonymous")
     text = models.TextField()
 
     def __str__(self):
