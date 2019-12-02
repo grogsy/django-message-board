f5759c1c0fcaba338e8209f8a70caa0fe483ce82
Brian <a.gray.cloud@gmail.com>
2019-12-01 21:46:56 -0400

    add a title field to post

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    title = models.CharField(max_length=54)
     text = models.TextField()
 
     def __str__(self):
-        return self.text[:50]
+        return self.title
