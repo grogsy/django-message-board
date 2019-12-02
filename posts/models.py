5d068ace332507a93fc10f4cc8e99347e46fa8c1
Brian <a.gray.cloud@gmail.com>
2019-12-01 21:49:39 -0400

    migrate posts db to enable titles

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=54)
+    title = models.CharField(max_length=54, default='New Post')
     text = models.TextField()
 
     def __str__(self):
